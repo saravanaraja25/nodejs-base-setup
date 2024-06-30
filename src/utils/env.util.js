b840cb7664b99a8d4ec6227ed60e11e4bcf8f65b
Saravana Raja <saravanarajapp@presidio.com>
2024-06-30 13:34:55 +0530

    eslint fixes

--- src/utils/env.util.js
+++ src/utils/env.util.js
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const getEnv = (key) => {
-    return process.env[key];
+  return process.env[key];
 };
--- src/utils/logger.util.js
+++ src/utils/logger.util.js
@@ -1,10 +1,10 @@
 import winston from 'winston';
 
 const logger = winston.createLogger({
-    level: 'debug',
-    format: winston.format.json(),
-    defaultMeta: { service: 'hr-insights-api' },
-    transports: [new winston.transports.Console()],
+  level: 'debug',
+  format: winston.format.json(),
+  defaultMeta: { service: 'hr-insights-api' },
+  transports: [new winston.transports.Console()],
 });
 
 export default logger;
